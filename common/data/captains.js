60bbdc067b7d23811e26b81a29773bcc8bd01254
optc-sp <optcsp@gmail.com>
2016-11-26 18:47:53 +0100

    Update captains.js

--- common/data/captains.js
+++ common/data/captains.js
@@ -2864,7 +2864,7 @@ window.captains = {
     },
     1131: {
         atk: function(p) { return p.unit.class.has("Fighter") || p.unit.class.has("Powerhouse") ? 1.5 : 1; },
-        hp: function(p) { return 2; }
+        hp: function(p) { return p.unit.class.has("Fighter") || p.unit.class.has("Powerhouse") ? 2 : 1; }
     },
     1132: {
         atk: function(p) { return p.unit.class.has("Fighter") ? 1.5 : 1; },
@@ -3146,7 +3146,7 @@ window.captains = {
     1221: {
         atk: function(p) { return p.unit.class.has("Slasher") ? 1.75 : 1; },
         hp: function(p) { return p.unit.class.has("Powerhouse") ? 1.2 : 1; },
-    },
+    }, 
     1222: {
         atk: function(p) { return p.unit.class.has("Slasher") ? 2 : 1; },
         hp: function(p) { return p.unit.class.has("Powerhouse") ? 1.75 : 1; },
@@ -3551,10 +3551,10 @@ window.captains = {
     },
     1315: {
         hitAtk: function(p) {
-            return p.modifiers.slice(0, p.chainPosition).subcontains(["Perfect", "Perfect", "Perfect", "Perfect", "Perfect"]) ? 3 : 
+            return p.modifiers.slice(0, p.chainPosition).subcontains(["Perfect", "Perfect", "Perfect", "Perfect", "Perfect"]) ? 3.5 : 
             //p.modifiers.slice(0, p.chainPosition).subcontains(["Perfect", "Perfect", "Perfect", "Perfect"]) ? 3 :
             //p.modifiers.slice(0, p.chainPosition).subcontains(["Perfect", "Perfect", "Perfect"]) ? 2.75 :
-            p.modifiers.slice(0, p.chainPosition).subcontains(["Perfect", "Perfect"]) ? 2 : 1;
+            p.modifiers.slice(0, p.chainPosition).subcontains(["Perfect", "Perfect"]) ? 2.5 : 1;
         },
         hitModifiers: ["Perfect", "Perfect", "Perfect", "Perfect", "Perfect", "Perfect"]
     },
@@ -3653,4 +3653,38 @@ window.captains = {
         },
         hitModifiers: ["Below Good", "Below Good", "Below Good", "Perfect", "Perfect", "Perfect"]
     },
+    1342: {
+        hp: function(p) { return p.unit.type == "PSY" ? 1.75 : 1; }
+    },
+    1343: {
+        hp: function(p) { return 0.5; },
+        atk: function(p) { return p.unit.class.has("Driven") ? 2 : 1; },
+    },
+    1344: {
+        hp: function(p) { return 0.5; },
+        atk: function(p) { return p.unit.class.has("Driven") ? 2.25 : 1; },
+    },
+    1345: {
+        atk: function(p) { return p.unit.class.has("Fighter") ? 2.5 : 1; },
+    },
+    1346: {
+        hp: function(p) { return p.unit.class.has("Shooter") || p.unit.class.has("Cerebral") ? 1.5 : 1; },
+        atk: function(p) { return p.unit.class.has("Shooter") || p.unit.class.has("Cerebral") ? 1.5 : 1; },
+        rcv: function(p) { return p.unit.class.has("Shooter") || p.unit.class.has("Cerebral") ? 1.5 : 1; },
+    },
+    1349: {
+        atk: function(p) { return p.unit.class.has("Cerebral") ? 1.5 : 1; },
+        rcv: function(p) { return p.unit.class.has("Cerebral") ? 1.75 : 1; },
+    },
+    1352: {
+        damageSorter: function(d) { return CrunchUtils.classSort(d, 3.5, ["Fighter"]); },
+        hitAtk: function(p) {
+            if (!p.unit.class.has("Fighter")) return 1;
+            return p.modifiers.slice(0, p.chainPosition).subcontains(["Perfect", "Perfect", "Perfect", "Perfect", "Perfect"]) ? 3.5 : 2;
+        },
+        hitModifiers: ["Perfect", "Perfect", "Perfect", "Perfect", "Perfect", "Perfect"]
+    },
+    1353:{
+        atk: function(p) { return p.unit.class.has("Cerebral") ? 2.5 : 1; },
+    }
 };
