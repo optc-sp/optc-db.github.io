993c32bf12a9496e28c0906343f5b1058b2f3c8e
optc-sp <optcsp@gmail.com>
2016-12-06 13:55:18 +0100

    Update tandems.js

--- common/data/tandems.js
+++ common/data/tandems.js
@@ -1,332 +1,332 @@
 window.tandems = [
     {
         "name": "Gum-Gum Mouton Triple-Sword Meteor",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos",
+        "desc": "Deals 1,000 fixed damage to all enemies",
         "units": [ 17, 2, 5, 13 ]
     },
     {
         "name": "Gum-Gum Dragon Epaule Whip Twister Shot",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a los enemigos de la columna del frente",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to the enemies in the front column",
         "units": [ 3, 6, 18 ]
     },
     {
         "name": "Three-Sword Style Gum-Gum Diable Jambe Mouton Jet 600 Pound Cannon",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to all enemies",
         "units": [ 8, 20, 4 ]
     },
     {
         "name": "Trinity Slash",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a los enemigos de la columna del frente",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to the enemies in the front column",
         "units": [ 47, 48, 5 ]
     },
     {
         "name": "Tornado Tabasco Pound Phoenix",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a los enemigos de la columna trasera",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to the enemies in the back column",
         "units": [ 10, 14, 7 ]
     },
     {
         "name": "Weak Trio/Guts Attack",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a los enemigos de la columna del frente",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to the enemies in the front column",
         "units": [ 9, 13, 21 ]
     },
     {
         "name": "Golden Mirage Shot",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 15, 11, 19 ]
     },
     {
         "name": "Thunder Horn Fire Bird Star",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a los enemigos de la columna trasera",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to the enemies in the back column",
         "units": [ 12, 25, 16 ]
     },
     {
         "name": "600,000,000 Beli Jackpot",
-        "desc": "Corta el HP de cada enemigo en un 5%",
+        "desc": "Cuts the current HP of each enemy by 5%",
         "units": [ 709, 739, 20, 8, 4 ]
     },
     {
         "name": "Penguin Reindeer Triangle",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a los enemigos de la columna trasera",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to the enemies in the back column",
         "units": [ 81, 82, 23 ]
     },
     {
         "name": "Rare Animal Miracle Attack",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a los enemigos de la columna del frente",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to the enemies in the front column",
         "units": [ 40, 26, 83, 118 ]
     },
     {
         "name": "Bandit Triple Attack",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a los enemigos de la columna del frente",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to the enemies in the front column",
         "units": [ 119, 120, 27 ]
     },
     {
         "name": "Slip-Slip Circus Carnival",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 37, 36, 38, 30 ]
     },
     {
         "name": "Slip-Slip Circus Festival",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to all enemies",
         "units": [ 37, 36, 39, 30 ]
     },
     {
         "name": "Navy Admiral Union Buster",
-        "desc": "Hace 5x ATK del ultimo personaje del tandem a los enemigos de la columna del frente",
+        "desc": "Deals 5 times the ATK of the last character in the tandem to the enemies in the front column",
         "units": [ 59, 49, 35 ]
     },
     {
         "name": "Buggy Circus Carnival",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 36, 37, 38 ]
     },
     {
         "name": "Buggy Circus Festival",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 36, 37, 39 ]
     },
     {
         "name": "Black Cat Formation X",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 41, 42, 43, 45 ]
     },
     {
         "name": "Black Cat Formation Z",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to all enemies",
         "units": [ 41, 42, 43, 46 ]
     },
     {
         "name": "Baratie's Famous Surf 'n Turf",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to all enemies",
         "units": [ 50, 51, 52 ]
     },
     {
         "name": "Krieg Fleet Delta Attack",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 55, 54, 57 ]
     },
     {
         "name": "Krieg Fleet Delta Hurricane",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 55, 54, 58 ]
     },
     {
         "name": "Krieg Fleet Delta Burst",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 56, 54, 57 ]
     },
     {
         "name": "Krieg Fleet Delta Explosion",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to all enemies",
         "units": [ 56, 54, 58 ]
     },
     {
         "name": "Fishman Smash Storm",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 61, 63, 62, 65 ]
     },
     {
         "name": "Fishman Crash Storm",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 61, 63, 62, 66 ]
     },
     {
         "name": "Fishman Burst Cannon",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to all enemies",
         "units": [ 61, 64, 62, 65 ]
     },
     {
         "name": "Fishman Ultra Burst Cannon",
-        "desc": "Hace 3x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to all enemies",
         "units": [ 61, 64, 62, 66 ]
     },
     {
         "name": "Penguin Triangle",
-        "desc": "Hace 2x ATK del ultimo personaje del tandem a los enemigos de la columna del frente",
+        "desc": "Deals 2 times the ATK of the last character in the tandem to the enemies in the front column",
         "units": [ 78, 80, 79 ]
     },
     {
         "name": "Penguin Pentagon",
-        "desc": "Hace 5x ATK del ultimo personaje del tandem a todos los enemigos",
+        "desc": "Deals 5 times the ATK of the last character in the tandem to all enemies",
         "units": [ 84, 86, 85, 87, 88 ]
     },
     {
         "name": "Red Combination",
-        "desc": "Hace 1000 en daño fijo a los enemigos en la columna del frente",
+        "desc": "Deals 1,000 fixed damage to the enemies in the front column",
         "units": [ 121, 126, 131, 136 ]
-    }
+    },
     {
         "name": "Blue Combination",
-        "desc": "Hace 1000 en daño fijo a los enemigos en la columna del frente",
+        "desc": "Deals 1,000 fixed damage to the enemies in the front column",
         "units": [ 122, 127, 132, 137 ]
     },
     {
         "name": "Green Combination",
-        "desc": "Hace 1000 en daño fijo a los enemigos en la columna del frente",
+        "desc": "Deals 1,000 fixed damage to the enemies in the front column",
         "units": [ 123, 128, 133, 138 ]
     },
     {
         "name": "Yellow Combination",
-        "desc": "Hace 1000 en daño fijo a los enemigos en la columna del frente",
+        "desc": "Deals 1,000 fixed damage to the enemies in the front column",
         "units": [ 124, 129, 134, 139 ]
     },
     {
         "name": "Black Combination",
-        "desc": "Hace 1000 en daño fijo a los enemigos en la columna del frente",
+        "desc": "Deals 1,000 fixed damage to the enemies in the front column",
         "units": [ 125, 130, 135, 140 ]
     },
     {
         "name": "Red Formation",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos",
+        "desc": "Deals 1,000 fixed damage to all enemies",
         "units": [ 142, 147, 152, 157 ]
     },
     {
         "name": "Blue Formation",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos",
+        "desc": "Deals 1,000 fixed damage to all enemies",
         "units": [ 143, 148, 153, 158 ]
     },
     {
         "name": "Green Formation",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos",
+        "desc": "Deals 1,000 fixed damage to all enemies",
         "units": [ 144, 149, 154, 159 ]
     },
     {
         "name": "Yellow Formation",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos",
+        "desc": "Deals 1,000 fixed damage to all enemies",
         "units": [ 145, 150, 155, 160 ]
     },
     {
         "name": "Black Formation",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos",
+        "desc": "Deals 1,000 fixed damage to all enemies",
         "units": [ 146, 151, 156, 161 ]
     },
     {
         "name": "Black Cat Formation C",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos",
+        "desc": "Deals 1,000 fixed damage to all enemies",
         "units": [ 165, 166, 167, 168, 169 ]
     },
     {
         "name": "Absolute Justice Formation",
-        "desc": "Hace 1000 en daño fijo a los enemigos en la columna del frente",
+        "desc": "Deals 1,000 fixed damage to the enemies in the front column",
         "units": [ 179, 180, 181, 182, 183 ]
     },
     {
         "name": "Absolute Justice Hammer",
-        "desc": "Hace 2000 en daño fijo a todos los enemigos",
+        "desc": "Deals 2,000 fixed damage to all enemies",
         "units": [ 184, 185, 186, 187, 188 ]
     },
     {
         "name": "Absolute Justice Judgment",
-        "desc": "Hace 2000 en daño fijo a todos los enemigos",
+        "desc": "Deals 2,000 fixed damage to all enemies",
         "units": [ 194, 195, 196, 197, 198 ]
     },
     {
         "name": "Billion Tricks",
-        "desc": "Hace 2000 en daño fijo a todos los enemigos",
+        "desc": "Deals 2,000 fixed damage to all enemies",
         "units": [ 245, 246, 242, 244, 243 ]
     },
     {
         "name": "Vow of Red",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos, regenera 500 HP",
+        "desc": "Deals 1,000 fixed damage to all enemies, recovers 500 HP",
         "units": [ 269, 274, 279, 284 ]
     },
     {
         "name": "Vow of Blue",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos, regenera 500 HP",
+        "desc": "Deals 1,000 fixed damage to all enemies, recovers 500 HP",
         "units": [ 270, 275, 280, 285 ]
     },
     {
         "name": "Vow of Green",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos, regenera 500 HP",
+        "desc": "Deals 1,000 fixed damage to all enemies, recovers 500 HP",
         "units": [ 271, 276, 281, 286 ]
     },
     {
         "name": "Vow of Yellow",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos, regenera 500 HP",
+        "desc": "Deals 1,000 fixed damage to all enemies, recovers 500 HP",
         "units": [ 272, 277, 282, 287 ]
     },
     {
         "name": "Vow of Black",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos, regenera 500 HP",
+        "desc": "Deals 1,000 fixed damage to all enemies, recovers 500 HP",
         "units": [ 273, 278, 283, 288 ]
     },
     {
         "name": "Foreman Triple Attack",
-        "desc": "Hace 2000 en daño fijo a todos los enemigos, amplifica el efecto de los orbes x1.25 durante 1 turno",
+        "desc": "Deals 2,000 fixed damage to all enemies, amplifies the effect of orbs by 1.25x for 1 turn",
         "units": [ 325, 628, 320 ]
     },
     {
         "name": "Trinity Attack",
-        "desc": "Hace 2x el ATK acumulado de todos los personajes en el tandem a todos los enemigos, multiplica el ATK de los personajes PSY x1.4 hasta el final del turno actual",
+        "desc": "Deals 2 times the cumulative ATK of all the characters in the tandem to all enemies, boosts ATK of PSY characters by 1.2x until the end of the current turn",
         "units": [ 338, 339, 336 ]
     },
     {
         "name": "Super Trinity Attack",
-        "desc": "Hace 2x el ATK acumulado de todos los personajes en el tandem a todos los enemigos, multiplica el ATK de los personajes PSY x1.4 hasta el final del turno actual",
+        "desc": "Deals 2 times the cumulative ATK of all the characters in the tandem to all enemies, boosts ATK of PSY characters by 1.4x until the end of the current turn",
         "units": [ 338, 339, 337 ]
     },
     {
         "name": "Super Quartet Attack",
-        "desc": "Hace 2x el ATK acumulado de todos los personajes en el tandem a todos los enemigos, multiplica el ATK de los personajes PSY x1.5 hasta el final del turno actual",
+        "desc": "Deals 2 times the cumulative ATK of all the characters in the tandem to all enemies, boosts ATK of PSY characters by 1.5x until the end of the current turn",
         "units": [ 339, 338, 614, 337 ]
     },
     {
         "name": "Maiden's Awakening: Do you like floral prints?",
-        "desc": "Hace 1x el ATK acumulado de todos los personajes en el tandem a todos los enemigos, regenera 1180 HP, multiplica el ATK de todos los personajes x1.2 hasta el final del turno actual",
+        "desc": "Deals the cumulative ATK of all the characters in the tandem to all enemies, recovers 1,180 HP, boosts ATK of all characters by 1.2x until the end of the current turn",
         "units": [ 436, 433, 434, 390 ]
     },
     {
         "name": "Light Shandia's Fire!",
-        "desc": "Hace 1000 en daño fijo a todos los enemigos, multiplica el ATK de todos los personajes x1.3 hasta el final del turno actual",
+        "desc": "Deals 1,000 fixed damage to all enemies, boosts ATK of all characters by 1.3x until the end of the current turn",
         "units": [ 466, 581, 582, 544 ]
     },
     {
         "name": "Blessings of Vearth",
-        "desc": "Regenera 2000 HP",
+        "desc": "Recovers 2,000 HP",
         "units": [ 544, 583, 584 ]
     },
     {
         "name": "Monster Burger",
-        "desc": "Hace 3000 en daño fijo a todos los enemigos, amplifica el efecto de los orbes x1.25 durante 1 turno",
+        "desc": "Deals 3,000 fixed damage to all enemies, amplifies the effect of orbs by 1.25x for 1 turn",
         "units": [ 565, 566, 567 ]
     },
     {
         "name": "Everything Cannon",
-        "desc": "Hace 3x el ATK del último personaje del tandem a los enemigos en la columna del frente, reduce el daño recibido en un 20% durante 1 turno",
+        "desc": "Deals 3 times the ATK of the last character in the tandem to the enemies in the front column, reduces damage received by 20% for 1 turn",
         "units": [ 615, 616, 617, 614 ]
     },
     {
         "name": "Super Everything Cannon",
-        "desc": "Hace 7x el ATK del último personaje a todos los enemigos",
+        "desc": "Deals 7 times the ATK of the last character in the tandem to all enemies",
         "units": [ 614, 615, 616, 617, 629 ]
     },
     {
         "name": "Fishman Fearsome Triple Combo",
-        "desc": "Hace 2x el ATK acumulado de todos los personajes en el tandem a todos los enemigos, multiplica el ATK de los personajes STR x1.3 durante 1 turno",
+        "desc": "Deals 2 times the cumulative ATK of all the characters in the tandem to all enemies, boosts the ATK of STR characters by 1.3x for 1 turn",
         "units": [ 657, 658, 656 ]
     },
     {
         "name": "Absolute Justice Divine Punishment",
-        "desc": "Hace 3000 en daño fijo a todos los enemigos",
+        "desc": "Deals 3,000 fixed damage to all enemies",
         "units": [ 696, 697, 698, 699, 700 ]
     },
     {
         "name": "Lightning Bone Sword: Gavotte Bond en Avant",
-        "desc": "Corta el HP de todos los enemigos en un 4%",
+        "desc": "Cuts the current HP of each enemy by 4%",
         "units": [ 12, 15, 709, 896 ]
     },
     {
         "name": "Giant Pirates' Charge",
-        "desc": "Hace 3x el ATK acumulado de todos los personajes en el tandem a todos los enemigos, multiplica el ATK de los personajes Powerhouse x1.4 hasta el final del turno actual",
+        "desc": "Deals 3 times the cumulative ATK of all the characters in the tandem to all enemies, boosts ATK of Powerhouse characters by 1.4x until the end of the current turn",
         "units": [ 884, 903 ]
     },
     {
         "name": "Kidnapping Trio Rampage",
-        "desc": "Hace 3000 en daño fijo a todos los enemigos",
+        "desc": "Deals 3,000 fixed damage to all enemies",
         "units": [ 954, 955, 956 ]
     },
     {
         "name": "Snake Pit Dance",
-        "desc": "Hace 3x el ATK acumulado de todos los personajes en el tandem a todos los enemigos, multiplica el ATK de los personajes Powerhouse x1.3 hasta el final del turno actual",
+        "desc": "Deals 3 times the cumulative ATK of all the characters in the tandem to all enemies, boosts ATK of Powerhouse characters by 1.3x until the end of the current turn",
         "units": [ 1056, 1058 ]
     },
 ];
